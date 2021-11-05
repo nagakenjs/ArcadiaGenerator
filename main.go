21ee065d10b065ffc53acd061e18460fbff1e241
Kenji Nagase <androxion@gmail.com>
2021-11-06 01:27:20 +0900

    コマンドラインの解析処理を実装

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("hello Go")
+	var (
+		authorArg  = flag.String("a", "", "auther name and Japanese author name")
+		titleArg   = flag.String("t", "", "title name and Japanese title name")
+		titleIdArg = flag.Int("i", 0, "title ID")
+	)
+	flag.Parse()
+
+	author, authorJp := splitArg(*authorArg)
+	title, titleJp := splitArg(*titleArg)
+	id := *titleIdArg
+
+	fmt.Println(author, authorJp, title, titleJp, id)
+}
+
+func splitArg(arg string) (string, string) {
+	args := strings.Split(arg, ":")
+	arg0 := args[0]
+	arg1 := arg0
+	if len(args) > 1 {
+		arg1 = args[1]
+	}
+	return arg0, arg1
 }
